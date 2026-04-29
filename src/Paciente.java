5b8af56f20181debe530c54f1a7e9f6c2b0e4e58
didirp03 <rodriguezpinillos25@iespabloserrano.com>
2026-04-29 21:44:39 +0200

    Add files via upload

---
+++ src/Paciente.java
@@ -0,0 +1,61 @@
+/**
+ * Clase que representa a un paciente en el sistema hospitalario
+ */
+import java.util.ArrayList;
+import java.util.List;
+public class Paciente {
+    private String nombre;
+    private String primerApellido;
+    private String segundoApellido;
+    private List<Ingreso> expediente;
+    public Paciente(String nombre, String primerApellido, String segundoApellido){
+        this.nombre = nombre;
+        this.primerApellido = primerApellido;
+        this.segundoApellido = segundoApellido;
+    }
+    public void generarNotaEvolucion(){
+    }
+    public void rondaMedicaDiaria(){
+    }
+    public void asignacion(){
+    }
+    public void traslado(){
+    }
+    public void evaluacionFinal(){
+    }
+    public void documentacionAltaPaciente(){
+    }
+    public void avanzarDia(){
+    }
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimerApellido() {
+        return primerApellido;
+    }
+
+    public void setPrimerApellido(String primerApellido) {
+        this.primerApellido = primerApellido;
+    }
+
+    public String getSegundoApellido() {
+        return segundoApellido;
+    }
+
+    public void setSegundoApellido(String segundoApellido) {
+        this.segundoApellido = segundoApellido;
+    }
+
+    public List<Ingreso> getExpediente() {
+        return expediente;
+    }
+
+    public void setExpediente(List<Ingreso> expediente) {
+        this.expediente = expediente;
+    }
+}
