abaffa4b20eee3641ba8c46fc74fabf0f88e0728
Wilson <ortizmino23@iespabloserrano.com>
2026-05-03 21:34:12 +0200

    Terminadas la programación de las clases Traslado, Alta y NotaEvolucion

--- src/edu.grupo2.proyectoreto/paciente/Alta.java
+++ src/edu.grupo2.proyectoreto/paciente/Alta.java
@@ -1,36 +1,156 @@
 package edu.grupo2.proyectoreto.paciente;
 
-import edu.grupo2.proyectoreto.utilidad.Fecha;
+import java.time.LocalDate;
 
 /**
- * Clase que representa el alta médica de un paciente correspondiente a un ingreso específico.
- * * El alta marca el final de la estancia de un paciente en el hospital bajo un ingreso determinado.
- * Contiene la información administrativa y temporal de este evento, y puede darse por 
- * diferentes motivos, principalmente por recuperación o por traslado.
- * * @author Grupo 2
+ * Representa el alta médica de un paciente en uno de sus ingresos.
+ * 
+ * El alta marca el final del ciclo de vida del paciente dentro del hospital.
+ * Un alta puede darse por recuperación o traslado.
+ * 
+ * @author Alex
+ * @author Wilson
  */
 public class Alta{
     /**
-     * Indica el motivo o tipo de alta del paciente.
+     * Tipo de alta basado en el motivo del alta del paciente.
+     * 
+     * Los valores permitidos son "RECUPERACIÓN" o "TRASLADO".
+     * No acepta null.
      */
     private String tipoAlta;
 
     /**
-     * Contiene la información detallada del traslado a otro centro médico.
-     * Este objeto solo contendrá información si el alta es de tipo "Traslado". 
-     * En caso de alta por recuperación, este atributo no se utilizará o será nulo.
+     * La fecha establecida en la que el paciente sera dado de alta.
+     * 
+     * No acepta null.
+     */
+    private LocalDate fechaAlta;
+
+    /**
+     * Texto correspondiente a la documentación del alta.
+     * 
+     * No acepta cadenas llenas de espacios.
+     */
+    private String documentacion;
+
+    /**
+     * Contiene la información del traslado a otro centro médico en caso de alta por traslado.
+     * 
+     * Este objeto solo contendrá información si el alta es de tipo "TRASLADO". 
+     * En caso de alta por recuperación, será null.
      */
     private Traslado traslado;
 
     /**
-     * La fecha en la que se ha establecido que el paciente sea dado de alta.
-     * Es establecida por el Médico tras una evaluación final exitosa.
+     * Crea un nuevo alta de tipo "RECUPERACIÓN".
+     * 
+     * @param fechaAlta fecha establecida en la que el paciente sera dado de alta, no acepta null
+     * 
+     * @throws NullPointerException si al parametro se le pasa null
      */
-    private Fecha fechaAlta;
+    Alta(LocalDate fechaAlta) {
+        if (fechaAlta == null) {
+            throw new NullPointerException("Parametro 'fechaAlta' no acepta null.");
+        }
+
+        this.fechaAlta = fechaAlta;
+        tipoAlta = "RECUPERACIÓN";
+    }
 
     /**
-     * Almacena los detalles administrativos, notas formales y el papeleo correspondiente al alta.
-     * Esta documentación es gestionada y rellenada por el Personal Administrativo 
+     * Crea un nuevo alta de tipo "TRASLADO".
+     * 
+     * @param fechaAlta       fecha establecida en la que el paciente sera dado de alta, no acepta null
+     * @param hospitalDestino nombre del hospital al cual se trasladara al paciente, no puede estar vacio ni ser null
+     * @param motivo          motivo del traslado, no puede estar vacío ni ser null
+     * 
+     * @throws NullPointerException     si 'fechaAlta','hospitalDestino' o 'motivo' se les pasa null
+     * @throws IllegalArgumentException si 'hospitalDestino' o 'motivo' se les pasa una cadena vacia, o
+     *                                  si 'hospitalDestino' o 'motivo' se les pasa una cadena llena de espacios
      */
-    private String documentacion;
+    Alta(LocalDate fechaAlta, String hospitalDestino, String motivo) {
+        if (fechaAlta == null) {
+            throw new NullPointerException("Parametro 'fechaAlta' no acepta null.");
+        }
+
+        this.fechaAlta = fechaAlta;
+        tipoAlta = "TRASLADO";
+
+        traslado = new Traslado(hospitalDestino, motivo);
+    }
+
+    /**
+     * Devuelve el tipo de alta.
+     * 
+     * @return el tipo de alta
+     */
+    String getTipoAlta() { return tipoAlta; }
+
+    /**
+     * Devuelve la fecha en la que el paciente sera dado de alta.
+     * 
+     * @return la fecha en la que el paciente sera dado de alta
+     */
+    LocalDate getFechaAlta() { 
+        return fechaAlta; 
+    }
+
+    /**
+     * Establece la fecha en la que el paciente sera dado de alta.
+     * 
+     * @param fechaAlta fecha establecida en la que el paciente sera dado de alta, no acepta null
+     * 
+     * @throws NullPointerException si al parametro se le pasa null
+     */
+    void setFechaAlta(LocalDate fechaAlta) {
+        if (fechaAlta == null) {
+            throw new NullPointerException("Parametro 'fechaAlta' no acepta null.");
+        }
+
+        this.fechaAlta = fechaAlta;
+    }
+
+    /**
+     * Devuelve la documentación del alta.
+     * 
+     * @return la documentación del alta
+     */
+    String getDocumentacion() {
+        return documentacion;
+    }
+
+    /**
+     * Establece la documentación del alta.
+     * 
+     * @param documentacion documentación del alta, no acepta cadenas llenas de espacios
+     * 
+     * @throws IllegalArgumentException si se pasa por parametro una cadena llena de espacios
+     */
+    void setDocumentacion(String documentacion) {
+        if (documentacion.isBlank()) {
+            throw new IllegalArgumentException("Parametro 'documentacion' no acepta una cadena con solo espacios.");
+        }
+
+        this.documentacion = documentacion;
+    }
+
+    /**
+     * Devuelve una cadena con todos los datos del alta.
+     * 
+     * Se muestran los datos según la convención de java para los métodos toString:
+     * - NombreClase{campo1='valor1', campo2='valor2'}
+     * 
+     * @return una cadena con todos los atributos y sus valores del alta
+     */
+    @Override
+    public String toString() {
+        return String.format(
+            "Alta{tipoAlta='%s', fechaAlta=%s, documentacion='%s', traslado=%s}",
+            tipoAlta,
+            fechaAlta,
+            documentacion,
+            traslado
+        );
+    }
 }
--- src/edu.grupo2.proyectoreto/paciente/NotaEvolucion.java
+++ src/edu.grupo2.proyectoreto/paciente/NotaEvolucion.java
@@ -1,51 +1,115 @@
-// TODO: Que Wilson Revise y corrija lo que hizo Jaime
-// TODO: Terminar la documentación de la clase, el constructor y los métodos
-// TODO: Corregir Override de toString()
+package edu.grupo2.proyectoreto.paciente;
 
 import java.time.LocalDate;
 
 /**
- * Las instancias de está clase representan la información referente a las notas de evolucion del
- * de un paciente.
+ * Representa la información referente a las notas de evolucion de los ingresos de un paciente.
  * 
  * @author Jaime
  * @author Wilson
  */
-public class NotaEvolucion {
+final class NotaEvolucion {
 
+    /**
+     * Fecha en la que se escibio la nota de evolución.
+     * 
+     * No acepta null.
+     */
     private LocalDate fecha;
+
+    /**
+     * Mensaje que quiso trasmitir el personal que hizo la nota de evolución.
+     * 
+     * No acepta null.
+     * No acepta cadena llena de espacios.
+     */
     private String observacion;
 
-    public NotaEvolucion(LocalDate fecha, String observacion) {
+    /**
+     * Crea una nueva nota de evolución con los datos especificados por parametro.
+     * 
+     * @param fecha       fecha en la que se escribio la nota de evolución, no acepta null
+     * @param observacion mensaje que se quiere trasmitir en la nota de evolución, 
+     *                    no acepta null ni cadena de espacios
+     * @throws NullPointerException      si 'fecha' o 'observación' se les pasa null
+     * @throws IllegalArgumentException si 'observacion' se le pasa una cadena llena de espacios
+     */
+    NotaEvolucion(LocalDate fecha, String observacion) {
+        if (fecha == null) {
+            throw new NullPointerException("Parametro 'fecha' no acepta null.");
+        }
 
+        if(observacion == null) {
+            throw new NullPointerException("Parametro 'observacion' no acepta null.");
+        }
+        if(observacion.isBlank()) {
+            throw new IllegalArgumentException("Parametro 'observacion' no acepta una cadena llena de espacios");
+        }
+        
         this.fecha = fecha;
         this.observacion = observacion;
     }
 
-    public LocalDate getFecha() {
-
+    /**
+     * Devuelve la fecha en la que se escribio la nota de evolución.
+     * 
+     * @return la fecha en la que se escribio la nota de evolución
+     */
+    LocalDate getFecha() {
         return fecha;
     }
 
-    public void setFecha(LocalDate fecha) {
+    /**
+     * Establece la fecha en la que se escribio la nota de evolución.
+     * 
+     * @param fecha fecha en la que se escribio la nota de evolución, no acepta null
+     * 
+     * @throws NullPointerException si al parametro se le pasa null
+     */
+    void setFecha(LocalDate fecha) {
+        if (fecha == null) {
+            throw new NullPointerException("Parametro 'fecha' no acepta null.");
+        }
+
         this.fecha = fecha;
     }
 
-    public String getObservacion() {
-
+    /**
+     * Devuelve la observación de la nota de evolución.
+     * 
+     * @return la observación de la nota de evolución.
+     */
+    String getObservacion() {
         return observacion;
     }
 
-    public void setObservacion(String observacion) {
-        
+    /**
+     * Establece la observación de la nota de evolución.
+     * 
+     * @param observacion mensaje que se quiere trasmitir en la nota de evolución, 
+     *                    no acepta null ni cadena de espacios
+     * 
+     * @throws NullPointerException     si al parametro se le pasa null
+     * @throws IllegalArgumentException si al parametro se le pasa una cadena llena de espacios
+     */
+    void setObservacion(String observacion) {
+        if(observacion == null) {
+            throw new NullPointerException("Parametro 'observacion' no acepta null.");
+        }
+
+        if(observacion.isBlank()) {
+            throw new IllegalArgumentException("Parametro 'observacion' no acepta una cadena llena de espacios");
+        }
+
         this.observacion = observacion;
     }
 
     @Override
     public String toString() {
-        return "NotaEvolucion{" +
-                "fecha='" + fecha + '\'' +
-                ", observacion='" + observacion + '\'' +
-                '}';
+        return String.format(
+            "NotaEvolucion{fecha=%s, observacion='%s'}",
+            fecha,
+            observacion
+        );
     }
 }
