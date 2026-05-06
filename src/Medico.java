6c5ee2cca94f62b36ddc48932ca43a3f75902c4c
didirp03 <rodriguezpinillos25@iespabloserrano.com>
2026-05-06 17:06:27 +0200

    Medico.java

--- src/Medico.java
+++ src/Medico.java
@@ -1,17 +1,28 @@
 /**
  * Clase que representa a un Médico en el sistema hospitalario.
- * Hereda de la clase abstracta PersonalDeSalud
+ * Hereda de la clase abstracta PersonalDeSalud y gestiona la evaluación final del paciente
  */
 public class Medico extends PersonalDeSalud{
     public Medico(){
-        //Llamamos al constructo portected de la clase padre (PersonalDeSalud)
+        //Llamamos al constructor portected de la clase padre (PersonalDeSalud)
         super();
     }
+    /**
+    *Realiza la evaluación técnica a los pacientes para determinar si pueden o no recibir el alta médica
+    *y que el personal administrativo trabaje posteriormente con ella
+    */
     public void evaluacionFinalPaciente(Paciente paciente, Fecha fechaAlata){
-        //Implementar la lógica de la evaluación final
-        //Pasos a seguir para crear la lógica:
-        //1: Crear el informa del alta (create)
-        //2: Actualizar el estado del paciente o su expediente (update)
-        //3: Finalizar o "destruir" el ingreso activo actual (destroy)
+     //Valdación
+        if(paciente != null) {
+            System.out.println("MÉDICO: Iniciando evaluación final para " + paciente.getNombre());
+            //Establecer la fecha del alta
+            //Aquí se asigna la fechaAlta al último ingreso registrado del paciente
+
+            //Modificar disponibilidad para administración
+            System.out.println("SISTEMA: Evaluación técnica positiva. El paciente está listo para el alta");
+            System.out.println("AVISO: Notificando al personal administrativo para procesar la documentación");
+        } else {
+            throw new IllegalArgumentException("El paciente no puede ser nulo");
+        }
     }
 }
