90abac8709e5956b78a7e83ed7fe389849b8d4c1
didirp03 <rodriguezpinillos25@iespabloserrano.com>
2026-05-06 17:34:02 +0200

    Cama.java

--- src/Cama.java
+++ src/Cama.java
@@ -10,9 +10,32 @@ public class Cama {
         this.disponibilidad = true;
         this.pacienteAsignado = null;
     }
+    /**
+    *Visibilidad de paquete.
+    *Una cama no puede asignarse si ya está ocupada
+    */
     public void asignacionPaciente (PersonalDeEnfermeria personal, Paciente paciente){
+        if(this.disponibilidad){
+            //Si está libre, asignamos y cambiamos estado
+            this.pacienteAsignado = paciente;
+            this.disponibilidad = false;
+            System.out.println("SISTEMA: Cama asignada al paciente " + paciente.getNombre());
+        } else {
+            throw new IllegalArgumentException("Error: La cama ya esta ocupada por otro paciente");
+        }
     }
+    /**
+    *Libera la cama para que pueda ser usada
+    */
     public void trasladoPaciente (PersonalDeEnfermeria personal, Paciente paciente){
+        if(this.pacienteAsignado != null && this.pacienteAsignado.equals(paciente)) {
+            //Liberamos la cama
+            this.pacienteAsignado = null;
+            this.disponibilidad = true;
+            System.out.println("SISTEMA: Cama liberada correctamente");
+        } else {
+            throw new IllegalArgumentException("Error: El paciente indicado no coincide con el ocupante de la cama");
+        }
     }
     public boolean isDisponibilidad(){
         return disponibilidad;
