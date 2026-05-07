29b2df9060cfd92597bfe0610915623b7f4b01e6
didirp03 <rodriguezpinillos25@iespabloserrano.com>
2026-05-07 17:16:44 +0200

    Medico.java

--- edu.grupo2.proyectoreto/personal/Medico.java
+++ edu.grupo2.proyectoreto/personal/Medico.java
@@ -2,10 +2,25 @@ package edu.grupo2.proyectoreto.personal;
 /**
  * Clase que representa a un Médico en el sistema hospitalario.
  * Hereda de la clase abstracta PersonalDeSalud y gestiona la evaluación final del paciente
+ *
+ *@author Diana
  */
 public class Medico extends PersonalDeSalud{
+    /**
+    * Poner comentarios
+    */
     public Medico(){
         //Llamamos al constructor portected de la clase padre (PersonalDeSalud)
         super();
+    }
+    @override
+    public String toString(){
+        return String.format(
+            "Medico{nombreMedico='%s', especialidadMedica='%s' " +
+            "observacionNota='%s'"},
+            nombreMedico,
+            especialidadMedica,
+            observacionNota
+        );
     }
 }
